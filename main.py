061d16b54517011045a3529aa2ee9151ad4cb8b6
Andrey <moregun@list.ru>
2023-08-19 14:30:03 +0300

    курсовая 5

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from src.utils import get_vacancies
+from src.utils import get_employers
 import json
 
 url = "https://api.hh.ru/employers"
 keyword = ['СБЕР', 'Яндекс', 'Тинькофф', 'Почта России', 'Ventra', 'Лаборатория Касперского', 'МегаФон', 'ВкусВилл', 'VK', 'СИБУР, Группа компаний']
-for el in keyword:
-    print(json.dumps(get_vacancies(el, url), ensure_ascii=False, indent=2))
+for employer in keyword:
+    print(json.dumps(get_employers(employer, url), ensure_ascii=False, indent=2))
--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,11 @@
 import requests
 import time
 import json
-#url = "https://api.hh.ru/employers"
-#keyword = 'ВкусВилл'
 
-
-def get_vacancies(keyword, url):
+def get_employers(keyword, url):
     """
     Получение списка вакансий с платформы HeadHunter
     """
-
-
     employers_lst = []
     for page in range(20):
         params = {'per_page': 100,
@@ -29,4 +24,39 @@ def get_vacancies(keyword, url):
 
 #print(json.dumps(get_vacancies(keyword, url), ensure_ascii=False, indent=2))
 
+def get_vacansies_by_employer(employer_id):
+    vacancies_lst = []
+    for page in range(20):
+        params = {'per_page': 100,
+                  'page': page,
+                  'employer': keyword,
+                  'search_field': 'name',
+                  'order_by': "publication_time",
+                  'archived': False,
+                  }
+        vacancies = requests.get("https://api.hh.ru/employers/3529", params=params).json()
+        vacancies_lst.extend(vacancies['items'])
+        return vacancies_lst
 
+def get_vacancies(url):
+    """
+    Получение списка вакансий с платформы HeadHunter
+    """
+    vacancies_lst = []
+    for page in range(5):
+        params = {'per_page': 100,
+                    'page': page,
+#                    'text': keyword,
+                    'search_field': 'name',
+                    'order_by': "publication_time",
+                    'archived': False,
+                    }
+        vacancies = requests.get(url, params=params).json()
+        vacancies_lst.extend(vacancies['items'])
+        if (vacancies['pages'] - page) <= 1:
+            break
+        time.sleep(0.5)
+    return vacancies_lst
+
+print(json.dumps(get_vacancies("https://api.hh.ru/vacancies?employer_id=3809"), ensure_ascii=False, indent=2))
+#rint(get_vacancies("https://api.hh.ru/vacancies?employer_id=3809"))
